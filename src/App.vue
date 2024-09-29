c425e6b1f7cf1ab91319866dfb19228131a357ab
Llerak <llerak.exe@gmail.com>
2024-09-29 10:05:48 -0400

    test sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { StatusUserLog } from './store/StatusUserLog';
+</script>
+
 <template>
-  <router-view />
+  <div class="flex w-[100vw] h-[100vh] relative">
+    <div
+      v-has-permission="'viewDashboard'"
+      class="absolute top-0 left-0 w-64 h-[100vh] bg-slate-600"
+    ></div>
+    <div>
+      <header></header>
+      <router-view />
+    </div>
+  </div>
+  <button
+    class="absolute bottom-0 right-1"
+    @click="() => console.log(StatusUserLog)"
+  >
+    test
+  </button>
 </template>
