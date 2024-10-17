0c88341a36b821cf47ab54971b6f3b20adb07d7d
XMKZ <emme.ko.zzz@gmail.com>
2024-10-17 15:37:08 -0400

    refactor refresh token services

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,30 +3,33 @@ import { roleAndUserGlobal } from '@/store/RolesAndPermission';
 import { statusApi } from '@/store/global';
 import { IUserLogin } from '@/interfaces';
 import { api } from '@/services';
+import { AxiosResponse } from 'axios';
 
 class AuthService {
   async login(user: IUserLogin) {
     statusApi.isLoading = true;
+    let response: AxiosResponse;
     try {
-      const response = await api.post(routeServices.auth.login, {
+      response = await api.post(routeServices.auth.login, {
         username: user.name,
         password: user.password,
       });
-      if (response.data) {
-        const data = response.data.response;
-        roleAndUserGlobal.name = user.name;
-        roleAndUserGlobal.role = data.role;
-        sessionStorage.setItem('Bearer', data.signinToken);
-        sessionStorage.setItem('RefreshBearer', data.refreshToken);
-        sessionStorage.setItem('Name', user.name);
-      }
-      statusApi.isLoading = false;
-      return response.data;
     } catch (error) {
       console.error('Error during login:', error);
       statusApi.isLoading = false;
       throw error;
     }
+    statusApi.isLoading = false;
+
+    if (response.data) {
+      const data = response.data.response;
+      roleAndUserGlobal.name = user.name;
+      roleAndUserGlobal.role = data.role;
+      sessionStorage.setItem('tk', data.signinToken);
+      sessionStorage.setItem('rsh_tk', data.refreshToken);
+      sessionStorage.setItem('Name', user.name);
+    }
+    return response.data;
   }
 
   logout() {
